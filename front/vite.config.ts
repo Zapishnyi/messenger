8cb64a1ab11f84534b22a1602d7b9169aac9f088
Zapishnyi <zapishny@ukr.net>
2025-04-06 14:57:06 +0300

    deploy to github pages added

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/messenger/',
 
-  server: {
-    port: 3000,
-    open: true,
-  },
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  // },
   build: {
     rollupOptions: {
       output: {
